be08fc2a0c2ee9b17d4fb3b3788c16f31e9350ec
Jordan <Jordan@Jordan-B-PC>
2012-10-25 21:42:58 -0700

    getting started

---
+++ AudioPlayer/src/jbull/gui/DefaultLayoutController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jbull.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import jbull.GUIController;
+
+/**
+ *
+ * @author Jordan
+ */
+public class DefaultLayoutController implements Initializable, GUIController {
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        System.out.println("controller supported");
+    }
+    
+}
